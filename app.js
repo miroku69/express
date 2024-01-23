9cc96bb6471998f3b1a13f0026a9606bf6447df1
miro <24kawakami36@gmail.com>
2024-01-23 15:19:38 +0900

    add

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
+var dogRouter = require('./routes/dog');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -21,6 +26,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
+app.use('/dog', dogRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
